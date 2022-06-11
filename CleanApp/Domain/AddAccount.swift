a51284cdf97ee5e98987df946df6f0d1f449c0d1
Gilberto Silva <betosilva.luz@gmail.com>
2022-06-11 19:30:55 -0300

    add AddAccount use case.

---
+++ CleanApp/Domain/AddAccount.swift
@@ -0,0 +1,22 @@
+//
+//  AddAccount.swift
+//  Domain
+//
+//  Created by Gilberto Silva on 11/06/22.
+//
+
+import Foundation
+
+protocol AddAccount {
+    func add(
+        addAccountModel: AddAccountModel,
+        completion: @escaping (Result<AccountModel, Error>) -> Void
+    )
+}
+
+struct AddAccountModel {
+    var name: String
+    var email: String
+    var password: String
+    var passwordConfirmation: String
+}
