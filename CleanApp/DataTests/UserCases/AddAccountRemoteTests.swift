98621adba6442f85fd910fc74fce66dd97300d5f
Gilberto Silva <betosilva.luz@gmail.com>
2022-06-12 21:55:59 -0300

    refactor simplify code

--- CleanApp/DataTests/UserCases/AddAccountRemoteTests.swift
+++ CleanApp/DataTests/UserCases/AddAccountRemoteTests.swift
@@ -12,7 +12,7 @@ import Data
 class AddAccountRemoteTests: XCTestCase {
     
     func test_add_should_call_httpClient_with_correct_url() throws {
-        let url = URL(string: "http://any-url.com")!
+        let url = makeUrl()
         let (sut, httpClientSpy) = makeSut(url: url)
         let addAccountModel = makeAddAccountModel()
         sut.add(addAccountModel: addAccountModel) { _ in }
@@ -46,7 +46,7 @@ class AddAccountRemoteTests: XCTestCase {
     func test_add_should_complete_with_error_if_client_completes_with_invalid_data() throws {
         let (sut, httpClientSpy)  = makeSut()
         expect(sut, completeWith: .failure(.unexpected), when: {
-            httpClientSpy.completeWithData(Data("invalid_data".utf8))
+            httpClientSpy.completeWithData(makeInvalidData())
         })
     }
 }
@@ -79,6 +79,14 @@ extension AddAccountRemoteTests {
         }
         action()
         wait(for: [exp], timeout: 1)
+    }
+    
+    func makeInvalidData() -> Data {
+        return Data("invalid_data".utf8)
+    }
+    
+    func makeUrl() -> URL {
+       return URL(string: "http://any-url.com")!
     }
     
     func makeAddAccountModel() -> AddAccountModel {
