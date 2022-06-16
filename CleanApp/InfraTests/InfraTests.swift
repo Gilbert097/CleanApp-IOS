c3c82c72744773582dca616e15e31a79379b8945
Gilberto Silva <betosilva.luz@gmail.com>
2022-06-15 21:47:04 -0300

    add infra layer

---
+++ CleanApp/InfraTests/InfraTests.swift
@@ -0,0 +1,15 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Gilberto Silva on 15/06/22.
+//
+
+import XCTest
+@testable import Infra
+
+class InfraTests: XCTestCase {
+    func test_() throws {
+        
+    }
+}
