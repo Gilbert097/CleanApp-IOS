442cce8587fb016406b12acdb55de65aaa331582
Gilberto Silva <betosilva.luz@gmail.com>
2022-09-11 19:06:17 -0300

    Add AddAccount integration test.

---
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -0,0 +1,35 @@
+//
+//  UseCasesIntegrationTests.swift
+//  UseCasesIntegrationTests
+//
+//  Created by Gilberto Silva on 11/09/22.
+//
+
+import XCTest
+import Infra
+import Data
+import Domain
+
+class AddAccountIntegrationTests: XCTestCase {
+
+    func test_add_account() throws {
+        let alamofireAdapter = AlamofireAdapter()
+        let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        let sut = AddAccountRemote(url: url, httpPostClient: alamofireAdapter)
+        let addAccountModel = AddAccountModel(name: "Gilberto Silva", email: "gilberto.silva.teste123@gmail.com", password: "secret", passwordConfirmation: "secret")
+        
+        let exp = expectation(description: "Waiting")
+        sut.add(addAccountModel: addAccountModel) { result in
+            switch result {
+            case .success(let account):
+                XCTAssertNotNil(account.id)
+                XCTAssertEqual(account.name, addAccountModel.name)
+                XCTAssertEqual(account.email, addAccountModel.email)
+            case .failure:
+                XCTFail("Expect succes got \(result) instead.")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 10)
+    }
+}
