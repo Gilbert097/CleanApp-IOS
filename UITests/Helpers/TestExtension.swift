8ce7534db53dea93d1c2c8ef0a0fdcbea078f62f
Gilberto Silva <betosilva.luz@gmail.com>
2022-11-15 20:41:00 -0300

    move extension to file.

---
+++ UITests/Helpers/TestExtension.swift
@@ -0,0 +1,24 @@
+//
+//  TestExtension.swift
+//  UITests
+//
+//  Created by Gilberto Silva on 15/11/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIControl {
+    
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach{ action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
+    }
+}
