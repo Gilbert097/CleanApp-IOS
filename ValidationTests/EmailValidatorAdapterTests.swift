f27b2d89ba664eeb548d2022ac67de7b22a7cb15
Gilberto Silva <betosilva.luz@gmail.com>
2023-03-13 17:52:47 -0300

    Add validation target.

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Gilberto Silva on 13/03/23.
+//
+
+import XCTest
+
+final class EmailValidatorAdapterTests: XCTestCase {
+
+    func test_() throws {
+        
+    }
+}
