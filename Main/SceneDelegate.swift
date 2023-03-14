b12e2a479a908e831c76c9afd811c2dde6ef52b1
Gilberto Silva <betosilva.luz@gmail.com>
2023-03-14 17:04:15 -0300

    Add Main target.

---
+++ Main/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  Main
+//
+//  Created by Gilberto Silva on 14/03/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
