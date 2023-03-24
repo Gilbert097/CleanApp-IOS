2ec541f8e57b151ad77e15b61e8f310d6ae44e4c
Gilberto Silva <betosilva.luz@gmail.com>
2023-03-24 18:13:39 -0300

    move sua creation to a factory method.

--- MainTests/Composers/SingUpComposerTests.swift
+++ MainTests/Composers/SingUpComposerTests.swift
@@ -7,11 +7,26 @@
 
 import XCTest
 import Main
+import UI
 
 final class SingUpComposerTests: XCTestCase {
     
-    func test_UI_presentation_integration() throws {
-        let sut =  SignUpComposer.composeViewControllerWith(addAccount: AddAccountSpy())
-        checkMemoryLeak(for: sut)
+    func test_background_request_should_complete_on_main_thread() throws {
+        let (sut, addAccountSpy) = makeSut()
+        sut.loadViewIfNeeded()
+        //        DispatchQueue.global().async {
+        //            addAccountSpy.completeWithError(.unexpected)
+        //        }
+    }
+}
+
+extension SingUpComposerTests{
+    
+    func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: SignUpViewController, addAccountSpy: AddAccountSpy) {
+        let addAccountSpy = AddAccountSpy()
+        let sut =  SignUpComposer.composeViewControllerWith(addAccount: addAccountSpy)
+        checkMemoryLeak(for: sut, file: file, line: line)
+        checkMemoryLeak(for: addAccountSpy, file: file, line: line)
+        return (sut, addAccountSpy)
     }
 }
